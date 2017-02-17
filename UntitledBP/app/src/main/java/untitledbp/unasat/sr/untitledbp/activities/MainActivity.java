65f614afef54b4e2d80dc78e795c25fbf592a175
JairJosafath <jairjosafath@gmail.com>
2017-02-17 00:46:36 -0300

    app

---
+++ UntitledBP/app/src/main/java/untitledbp/unasat/sr/untitledbp/activities/MainActivity.java
@@ -0,0 +1,17 @@
+package untitledbp.unasat.sr.untitledbp.activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import untitledbp.unasat.sr.untitledbp.R;
+import untitledbp.unasat.sr.untitledbp.db.dao.Dao;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Dao dao = new Dao(this);
+    }
+}
--- UntitledBP/app/src/main/java/untitledbp/unasat/sr/untitledbp/db/dao/Dao.java
+++ UntitledBP/app/src/main/java/untitledbp/unasat/sr/untitledbp/db/dao/Dao.java
@@ -1,8 +1,51 @@
 package untitledbp.unasat.sr.untitledbp.db.dao;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static untitledbp.unasat.sr.untitledbp.db.schema.Schema.DATABASE_NAME;
+import static untitledbp.unasat.sr.untitledbp.db.schema.Schema.DATABASE_VERSION;
+import static untitledbp.unasat.sr.untitledbp.db.schema.Schema.SchemaBalance.CREATE_BALTABLE;
+import static untitledbp.unasat.sr.untitledbp.db.schema.Schema.SchemaCategory.CREATE_CATTABLE;
+import static untitledbp.unasat.sr.untitledbp.db.schema.Schema.SchemaReport.CREATE_REPTABLE;
+import static untitledbp.unasat.sr.untitledbp.db.schema.Schema.SchemaTransaction.CREATE_TRANTABLE;
+import static untitledbp.unasat.sr.untitledbp.db.schema.Schema.SchemaUser.CREATE_USERTABLE;
+
 /**
  * Created by Jair on 2/16/2017.
  */
 
-public class Dao {
+public class Dao extends SQLiteOpenHelper{
+
+    public Dao(Context context) {
+
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+        db.execSQL(CREATE_USERTABLE);
+        db.execSQL(CREATE_TRANTABLE);
+        db.execSQL(CREATE_CATTABLE);
+        db.execSQL(CREATE_BALTABLE);
+        db.execSQL(CREATE_REPTABLE);
+
+       // setDefaultCategories();
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+
+    private void setDefaultCategories() {
+
+
+    }
+    //methods to use db
+
+
 }
